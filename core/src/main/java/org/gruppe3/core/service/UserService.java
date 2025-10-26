f7377e0d77174be1c0264366bc0d6cae4832deea
Stian Hetsch <hetsch@gmail.com>
2025-10-26 15:42:24 +0100

    CreateUserRequest / UserService opprettet

---
+++ core/src/main/java/org/gruppe3/core/service/UserService.java
@@ -0,0 +1,25 @@
+package org.gruppe3.core.service;
+
+import org.gruppe3.core.domain.User;
+import org.gruppe3.core.dto.CreateUserRequest;
+import org.gruppe3.core.exception.UserRepositoryException;
+import org.gruppe3.core.port.UserRepositoryPort;
+
+public class UserService {
+
+  private UserRepositoryPort userRepository;
+
+  public UserService(UserRepositoryPort userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public void createUser(CreateUserRequest request) throws UserRepositoryException {
+    User user =
+        new User(
+            request.getFirstName(),
+            request.getLastName(),
+            request.getPhoneNumber(),
+            request.getEmail());
+    userRepository.createUser(user);
+  }
+}
