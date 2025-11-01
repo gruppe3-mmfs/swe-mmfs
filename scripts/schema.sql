c87aaebb9c19ca1a146085d1e9775644bcfeb128
Stian Hetsch <hetsch@gmail.com>
2025-11-01 21:04:28 +0100

    Lagt til tabeller for bruker-familier

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,23 +1,30 @@
+CREATE TABLE IF NOT EXISTS prosjekt.family (
+	familyId INT AUTO_INCREMENT PRIMARY KEY,
+	fmailyName VARCHAR(50) NOT NULL,
+);
+
 CREATE TABLE IF NOT EXISTS prosjekt.users (
   userId INT AUTO_INCREMENT PRIMARY KEY,
   firstName VARCHAR(50) NOT NULL,
   lastName VARCHAR(50) NOT NULL,
-  phoneNumber VARCHAR(20) UNIQUE,
-  email VARCHAR(100) UNIQUE
+  phoneNumber VARCHAR(20) NOT NULL UNIQUE,
+  email VARCHAR(100) NOT NULL UNIQUE,
+	userFamilyId INT,
+	FOREIGN KEY (userFamilyId) REFERENCES prosjekt.family(familyId)
 );
 
 CREATE TABLE IF NOT EXISTS prosjekt.ticketTypes (
 	ticketTypeId INT AUTO_INCREMENT PRIMARY KEY,
-	ticketType VARCHAR(50) NOT NULL
+	ticketType VARCHAR(50) NOT NULL UNIQUE
 );
 
-INSERT INTO prosjekt.ticketTypes (ticketType) VALUES ("Normal");
-INSERT INTO prosjekt.ticketTypes (ticketType) VALUES ("Student");
-INSERT INTO prosjekt.ticketTypes (ticketType) VALUES ("Senior");
+INSERT IGNORE INTO prosjekt.ticketTypes (ticketTypeId, ticketType) VALUES (1, "Normal");
+INSERT IGNORE INTO prosjekt.ticketTypes (ticketTypeId, ticketType) VALUES (2, "Student");
+INSERT IGNORE INTO prosjekt.ticketTypes (ticketTypeId, ticketType) VALUES (3, "Senior");
 
 CREATE TABLE IF NOT EXISTS prosjekt.tickets (
 	ticketId INT AUTO_INCREMENT PRIMARY KEY,
-	ticketHash VARCHAR(50) NOT NULL,
+	ticketHash VARCHAR(64) NOT NULL,
 	ticketType INT NOT NULL,
 	ticketRouteOrigin VARCHAR(50) NOT NULL,
 	ticketRouteDestination VARCHAR(50) NOT NULL,
@@ -25,4 +32,3 @@ CREATE TABLE IF NOT EXISTS prosjekt.tickets (
 	FOREIGN KEY (ticketType) REFERENCES prosjekt.ticketTypes(ticketTypeId),
 	FOREIGN KEY (ticketOwnerId) REFERENCES prosjekt.users(userId)
 );
-
