9d864252b1ae47041a8aa671f3f2d7a6bb6737b9
Marius Aasgaard <aasgaardmarius@gmail.com>
2025-11-11 15:17:47 +0100

    Kommet godt på vei videre m/integrasjonstester

--- test/src/test/java/org/gruppe3/test/testdb/TestDatabase.java
+++ test/src/test/java/org/gruppe3/test/testdb/TestDatabase.java
@@ -14,44 +14,59 @@ public abstract class TestDatabase {
     // Oppretter user-tabellen
     public void createUserTable() throws Exception {
         try (Statement statement = connection.createStatement()) {
-            statement.execute("CREATE TABLE IF NOT EXISTS users "
-            + "(userId INT AUTO_INCREMENT PRIMARY KEY,"
-            + "firstName VARCHAR(25),"
-            + "lastName VARCHAR(25),"
-            + "honeNumber VARCHAR(15),"
-            + "email VARCHAR(50),"
-            + "userFamilyId INT,"
-            + "FOREIGN KEY (userFamilyId) REFERENCES family(familyId)");
+            statement.execute("CREATE TABLE IF NOT EXISTS users (\r\n" + //
+                                "  userId INT AUTO_INCREMENT PRIMARY KEY,\r\n" + //
+                                "  firstName VARCHAR(50) NOT NULL,\r\n" + //
+                                "  lastName VARCHAR(50) NOT NULL,\r\n" + //
+                                "  phoneNumber VARCHAR(20) NOT NULL UNIQUE,\r\n" + //
+                                "  email VARCHAR(100) NOT NULL UNIQUE,\r\n" + //
+                                "\tuserFamilyId INT,\r\n" + //
+                                "\tFOREIGN KEY (userFamilyId) REFERENCES family(familyId)\r\n" + //
+                                ")");
         }
     }
 
     // Oppretter ticket-tabellen
     public void createTicketTable() throws Exception {
         try (Statement statement = connection.createStatement()) {
-            statement.execute ("CREATE TABLE IF NOT EXISTS tickets "
-            + "(ticketId INT AUTO_INCREMENT PRIMARY KEY,"
-            + "ticketHash VARCHAR (64) NOT NULL UNIQUE,"
-            + "ticketTripOrigin VARCHAR (50) NOT NULL,"
-            + "ticketTripDestination VARCHAR (50) NOT NULL,"
-            + "ticketOwnerId INT, FOREIGN KEY (ticketType) REFERENCES ticketTypes(ticketTypeId)"
-            + "FOREIGN KEY (ticketOwnerId) REFERENCES users(userId)");
+            statement.execute ("CREATE TABLE IF NOT EXISTS tickets (\r\n" + //
+                                "\tticketId INT AUTO_INCREMENT PRIMARY KEY,\r\n" + //
+                                "\tticketHash VARCHAR(64) NOT NULL UNIQUE,\r\n" + //
+                                "\tticketType INT NOT NULL,\r\n" + //
+                                "\tticketTripOrigin VARCHAR(50) NOT NULL,\r\n" + //
+                                "\tticketTripDestination VARCHAR(50) NOT NULL,\r\n" + //
+                                "\tticketOwnerId INT,\r\n" + //
+                                "\tFOREIGN KEY (ticketType) REFERENCES ticketTypes(ticketTypeId),\r\n" + //
+                                "\tFOREIGN KEY (ticketOwnerId) REFERENCES users(userId)\r\n" + //
+                                ")");
         }
     }
 
     // Opprettet lookup-tabellen
     public void createLookupTable() throws Exception {
         try (Statement statement = connection.createStatement()) {
-            statement.execute ("CREATE TABLE IF NOT EXISTS ticketTypes "
-            + "(ticketTypeId INT AUTO_INCREMENT PRIMARY KEY,"
-            + "ticketType VARCHAR (59) NOT NULL UNIQUE,");
+            statement.execute ("CREATE TABLE IF NOT EXISTS ticketTypes (\r\n" + //
+                                "\tticketTypeId INT AUTO_INCREMENT PRIMARY KEY,\r\n" + //
+                                "\tticketType VARCHAR(50) NOT NULL UNIQUE\r\n" + //
+                                ")");
         }
     }
 
+    // Oppretter family-tabellen
+    public void createFamilyTable() throws Exception {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute ("CREATE TABLE IF NOT EXISTS family (\r\n" + //
+                                "\tfamilyId INT AUTO_INCREMENT PRIMARY KEY,\r\n" + //
+                                "\tfamilyName VARCHAR(50) NOT NULL\r\n" + //
+                                ")");
+        }
+    }    
+
     // Setter inn dummy-data i lookup-tabellen
     public void insertIntoLookupTable() throws Exception {
-        String sql1 = "INSERT IGNORE INTO ticketTypes (ticketTypeId, ticketType) VALUES (1, \"Normal\");";
-        String sql2 = "INSERT IGNORE INTO ticketTypes (ticketTypeId, ticketType) VALUES (2, \"Student\");";
-        String sql3 = "INSERT IGNORE INTO ticketTypes (ticketTypeId, ticketType) VALUES (3, \"Senior\");";
+        String sql1 = "INSERT INTO ticketTypes (ticketTypeId, ticketType) VALUES (1, 'Normal');";
+        String sql2 = "INSERT INTO ticketTypes (ticketTypeId, ticketType) VALUES (2, 'Student');";
+        String sql3 = "INSERT INTO ticketTypes (ticketTypeId, ticketType) VALUES (3, 'Senior');";
 
         try (Statement statement = connection.createStatement()) {
             statement.execute(sql1);
